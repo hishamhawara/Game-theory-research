361a202372b7607e2dd9a231a86f400a9d8b332c
Hisham Hawara <kayguxe@users.noreply.github.com>
2017-06-29 14:29:14 -0400

    update bid.py
    supports more than 2 bidders

--- bid.py
+++ bid.py
@@ -1,89 +1,79 @@
 import random
 import math
+import matplotlib.pyplot as plt
+
+number_of_players_string = input('how many players are you?')
+number_of_players = int(number_of_players_string)
 number_of_rounds = input('how many rounds do you want to play? ')
 T = int(number_of_rounds) #Number of round
-
+vi = []
 v1 = [0] + sorted([random.randint(0,2*T) for i in range(T)], reverse = True)
  #marginal valuation for bidder 1
 
 v2 = [0] + [2*T] + sorted([random.randint(0,T-1) for i in range(T-1)], reverse = True)
  #marginal valuation for bidder 2
-answer_question1 = input('player1 do you want to set your marginal values? (Y/N)')
-if answer_question1 == "Y" :
-    for i in range(T):
-        input_1 = input("value:")
-        v1[i] = int(input_1)
-answer_question2 = input('player2 do you want to set your marginal values? (Y/N)')
-if answer_question2 == "Y" :
-    for i in range(T):
-        input_2 = input("value:")
-        v2[i] = int(input_2)
+
+print(vi)
+for j in range(number_of_players):
+	answer_question1 = input('player' + str(j) + ' do you want to set your marginal values? (Y/N)')
+	if answer_question1 == "Y" :
+		v1[0] = 0 
+		for i in range(1,T+1):
+			input_1 = input("value:")
+			v1[i] = int(input_1)
+		vi.append(v1)
+	else:
+		v1 = [0] + sorted([random.randint(0,2*T) for i in range(T)], reverse = True)
+		vi.append(v1)
+#print(vi)
 
 #Check that v1/v2 are non-increasing ?
-
-vf1 = [0]*(T+1) #valuation function for bidder 1
-vf2 = [0]*(T+1) #                   for bidder 2
-
-for i in range(T + 1):
-    vf1[i] = vf1[i-1] + v1[i] #As vf1[-1] = 0, it's ok
-    vf2[i] = vf2[i-1] + v2[i] 
+vfi = [[] for i in range(number_of_players)] #valuation function for bidder i
 
 
-print(v1,vf1)
-print(v2,vf2)
-#End of inputs (is vf really an input ?)
+for j in range(number_of_players):
+	vfi[j] = [0]*(T+1)
 
+for i in range(T+1):
+	for j in range(number_of_players):
+		vfi[j][i] = vfi[j][i-1] + vi[j][i]
+print(vi)
+print(vfi)
 
 #Resolution
-
-utility_1 = 0 #Player 1 utility
-utility_2 = 0 #Player 2 utility
+utility_i = [0]*(number_of_players)
+nb_i = [0]*(number_of_players)
+bid_i = [0]*(number_of_players)
+for i in range(number_of_players):
+	utility_i[i] = 0
 
 price = [0]*T #Price of different items
-
-nb_1 = 0 #Number of items got by player 1
-nb_2 = 0 #Number of items got by player 2
+for i in range(number_of_players):
+	nb_i[i] = 0 #Number of items got by player i
 
 winner = 0 #winner for round i
 monopoly = 0
 win = []
 for i in range(1,T+1):
-    bid_1 = input('player 1 input your bid: ')
-    bid_2 = input('player 2 input your bid: ')
-    bid_1 = int(bid_1)
-    bid_2 = int(bid_2)
+	for j in range(number_of_players):
+		bid_i[j] = input('player' + str(j) + 'input your bid:')
+		bid_i[j] = int(bid_i[j])
     
-    price[i-1] = min(bid_1,bid_2)
-    
-    print("Round",i)
-    print("Bids:",bid_1,bid_2,'->',price[i-1])
-    
-    winner = 1*(bid_1 > bid_2) + 2*(bid_1 < bid_2)
-    win.append(winner)
-    
-    if(winner == 0): #In case of a tie
-        winner = random.randint(1,2)
-        win[-1] = str(winner)
-    print("Winner is",winner)
-    
-    if(winner == 1):
-        nb_1 += 1
-        utility_1 += v1[nb_1] - price[i-1]
-    else:
-        print(v2[nb_2])
-        print(price[i-1])
-        nb_2 += 1
-        utility_2 += v2[nb_2] - price[i-1]
-        
-    winner = 0
-    print("\nP1:",nb_1,"items, util",utility_1)
-    print("P2:",nb_2,"items, util",utility_2)
-    print("\n##############\n")
-    
+	m = max(bid_i)
+	price[i-1] = max(n for n in bid_i if n!=m)
+	print("Round",i)
+	print("Bids:",bid_i,'->',price[i-1])
+	winner = max(bid_i)
+	max_index = bid_i.index(winner)
+	win.append(winner)
+	print("Winner is",winner)
+	nb_i[max_index] += 1
+	print("price is = " + str(price[i-1]))
+	print("value is = " + str(vi[max_index][nb_i[max_index]]))
+	utility_i[max_index] += vi[max_index][nb_i[max_index]] - price[i-1]
+	print("\nP1:",nb_i,"items, util",utility_i)
+	print("\n##############\n")
 print(price)
 print(win)
-
-import matplotlib.pyplot as plt
-
 plt.plot(price)
 plt.show()

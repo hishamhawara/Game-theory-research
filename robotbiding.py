450453a67ef405dd673097b68ba62f4d2c1ab5a7
hishamhwr <hisham.hawara@mail.mcgill.ca>
2017-07-19 17:11:34 -0400

    Rprice + learning

---
+++ robotbiding.py
@@ -0,0 +1,110 @@
+import sys
+import random
+
+from tools import *
+
+seed = setseed()
+
+T = 10 #Number of round
+
+#marginal valuation for bidder 1
+v1 = vtype("uniform",T,low_b=T,up_b=2*T)
+#marginal valuation for bidder 2
+v2 = vtype("uniform",T,low_b=T,up_b=2*T)
+for i in range(10,len(v2)):
+    v2[i] -= T*3//4
+rprice = vtype("uniform",T,low_b=3,up_b=T)
+
+"""#marginal valuation for bidder 1
+v1 = vtype("custom",T,vals=[T+1]*T)
+#marginal valuation for bidder 2
+v2 = vtype("custom",T,vals=[T]*(T-1)+[0])
+"""
+
+
+#Check that v1/v2 are non-increasing ?
+
+vf1 = cumulative(v1) #valuation function for bidder 1
+vf2 = cumulative(v2) #                   for bidder 2
+
+print(v1)
+print(v2)
+print(rprice)
+#End of inputs (is vf really an input ?)
+
+
+value_1,value_2 = learn(v1,v2, T)
+
+#Resolution
+utility_1 = 0 #Player 1 utility
+utility_2 = 0 #Player 2 utility
+
+price = [0]*T #Price of different items
+
+nb_1 = 0 #Number of items got by player 1
+nb_2 = 0 #Number of items got by player 2
+
+bid_1 = [0]*T
+bid_2 = [0]*T
+bid_3 = [0]*T
+bid_4 = [0]*T
+monopoly = 0
+win = []
+
+
+for i in range(T):
+    winner = 0 #winner for round i
+    bid_1[i] = strat("learnR", 1, v=v1,  nb = nb_1, k = i, rprice = rprice)
+    #bid_1[i] = strat("greedy", 1, v1=v1, v2=v2, nb_1=nb_1, nb_2=nb_2, rnd_left=T-i)
+    bid_2[i] = strat("learnR", 2, v=v2,  nb = nb_2, k = i, rprice = rprice)
+    #bid_1[i] = strat("trial", 1, v1=v1, v2=v2, nb_1=nb_1, nb_2 = nb_2, k = i, bid1 = bid_1, bid2 = bid_2, rprice = rprice)
+    #bid_2[i] = strat("trial", 2, v1=v1, v2=v2, nb_1=nb_1, nb_2 = nb_2, k = i, bid1 = bid_1, bid2 = bid_2, rprice = rprice)
+    bid_3[i] = strat("learn", 1, v=v1,  nb = nb_1, k = i)
+    bid_4[i] = strat("learn", 2, v=v2,  nb = nb_2, k = i)
+
+    #bid_2[i] = strat("greedy", 2, v1=v1, v2=v2, nb_1=nb_1, nb_2=nb_2, rnd_left=T-i)
+
+    price[i] = min(bid_1[i], bid_2[i])
+    
+    if(v1[nb_1 + (T - i)] > v2[nb_2 + 1]):#If marginal value are decreasing, check if it is a monopoly
+        if(not monopoly):
+            monopoly = i
+        print("Round",i+1,"monopoly for player 1")
+        
+    elif (v2[nb_2 + (T - i)] > v1[nb_1 + 1]): 
+        if(not monopoly):
+            monopoly = i
+        print("Round",i+1,"monopoly for player 2")
+    else:
+        print("Round",i+1)
+    print("Bids:",bid_1[i],bid_2[i],'->',price[i])
+    
+    winner = 1*(bid_1[i] > bid_2[i]) + 2*(bid_1[i] < bid_2[i])
+    win.append(winner)
+    
+    if(winner == 0): #In case of a tie
+        winner = random.randint(1,2)
+        win[-1] = str(winner)
+    
+    print("Winner is",winner)
+    
+    if(winner == 1):
+        nb_1 += 1
+        utility_1 += v1[nb_1] - price[i]
+    else:
+        nb_2 += 1
+        utility_2 += v2[nb_2] - price[i]
+        
+    print("\nP1:",nb_1,"items, util",utility_1)
+    print("P2:",nb_2,"items, util",utility_2)
+    print("\n##############\n")
+    
+print("Price:",price)
+print("All winners:",win)
+print("Monopoly from",monopoly,"with results:",win[monopoly:])
+
+pivot(v1,v2,T)
+
+
+graph(bid_1,bid_2,v1,v2)
+graph(bid_3,bid_4,v1,v2)
---
+++ toolsedit.py
@@ -0,0 +1,222 @@
+import sys
+import matplotlib.pyplot as plt
+import random
+
+#set the seed given or generate a new seed
+def setseed(s=None): 
+    if(s == None):
+        s = random.randint(0,sys.maxsize)
+    random.seed(s)
+    print("Seed:",s)
+    return s
+
+def vtype(type,round,**args):
+    v = [0]
+
+    if(type == "uniform"): #Uniform values
+        low_b,up_b = args.get("low_b",0),args.get("up_b",2*round)
+        
+        
+        v += sorted([random.randint(low_b,up_b) for i in range(round)], reverse = True)
+    
+    elif(type == "cst-unif"): #Constant values for cst_t then uniform
+        low_b,up_b,cst_t = args.get("low_b",0), args.get("up_b",2*round), args.get("cst_t",round//2)
+        
+        v += [up_b]*cst_t
+        v += sorted([random.randint(low_b,up_b) for i in range(round - cst_t)], reverse = True)
+    
+    elif(type == "pow"): #pow > 1, more small value, pow < 1, more high value (shape as x^pow)
+        low_b,up_b,pow = args.get("low_b",0), args.get("up_b",2*round), args["pow"]
+        
+        v += sorted([ int(random.random()**(pow)*(up_b-low_b) + low_b) for i in range(round)], reverse = True)
+        
+    elif(type == "custom"):
+        vals = args["vals"]
+        v += sorted(vals, reverse = True)
+        
+    return v
+
+def cumulative(v):
+    return [sum(v[0:i+1]) for i in range(len(v))]
+
+
+def pivot(v1,v2,T):
+    v1 = v1.copy()
+    v2 = v2.copy()
+    v1[0] = 0
+    v2[0] = 0
+    vf1 = cumulative(v1) #valuation function for bidder 1
+    vf2 = cumulative(v2) #                   for bidder 2
+
+    profit1 = 0 
+    profitfo  = 0 
+    pivot_price = 0
+
+    profits1 = [0]*(T+1) #marginal valuation for bidder 1
+    profits2 = [0]*(T) #marginal valuation for bidder 1
+
+    profits1[0] = vf1[0]
+    for k in range(1,T+1):
+        profits1[k] = vf1[k] - (k * v2[T+1-k])
+    profit1 = max(profits1)
+    kstar = profits1.index(profit1)
+    print(kstar)
+    print("*********1111*****")
+    print(profit1)
+    print(profits1)
+    print("*********1111****")
+
+    profits2[0] = vf1[0]
+    for k in range(1, T):
+        profits2[k] = (vf1[k+1]-vf1[1]) - k * v2[T-k]
+    profitfo = max(profits2)
+    print("*********222222*********")
+    print(profitfo)
+    print(profits2)
+    print("*********222222*********")
+    #the pivot price calculation
+    pivot_price = v1[1] + profitfo - profit1
+    print("*********pivot price*****")
+    print(pivot_price)
+    print("*********pivot price****")
+
+
+
+def sgpe(vf1,vf2):
+    T = len(vf1) - 1
+    value_1 = [[0 for j in range(T - i + 1)] for i in range(T+1)] #initialization of vectors
+    value_2 = [[0 for j in range(T - i + 1)] for i in range(T+1)]
+    
+    for k1 in range(T + 1): #Initializing the value by the leaves
+        k2 = T - k1
+        value_1[k1][k2] = vf1[k1] 
+        value_2[k1][k2] = vf2[k2]
+        # print(k1,k2,value_1[k1][k2],value_2[k1][k2])
+    # print("End of depth",T)
+        
+    for i in range(T-1,-1,-1): #Applying recursion of internal nodes
+        for k1 in range(i + 1):
+            k2 = i - k1
+            
+            value_1[k1][k2] = (max(value_1[k1][k2+1], value_1[k1+1][k2] - (value_2[k1][k2+1] - value_2[k1+1][k2])))
+            value_2[k1][k2] = (max(value_2[k1+1][k2], value_2[k1][k2+1] - (value_1[k1+1][k2] - value_1[k1][k2+1])))
+    
+            # print(k1,k2,value_1[k1][k2],value_2[k1][k2])
+        # print("End of depth",i)
+    return value_1,value_2
+    
+
+
+
+
+
+def strat(type,bidder,**args):
+    if(type == "sgpe"):
+        value_1,value_2,nb_1,nb_2 = args["value_1"], args["value_2"],args["nb_1"],args["nb_2"]
+        
+        if(bidder == 1):
+            bid = value_1[nb_1+1][nb_2] - value_1[nb_1][nb_2+1]
+        else:
+            bid = value_2[nb_1][nb_2+1] - value_2[nb_1+1][nb_2]
+    
+    if(type == "greedy"):
+        v1,v2,nb_1,nb_2, rnd_left = args["v1"], args["v2"],args["nb_1"],args["nb_2"],args["rnd_left"]
+        
+        if(bidder == 1):
+            bid = pivot(v1[nb_1:],v2[nb_2:],rnd_left)
+        else:
+            bid = pivot(v2[nb_2:],v1[nb_1:],rnd_left)
+
+    if(type == "learnR"):
+        v, nb, i, rprice = args["v"],args["nb"], args["k"], args["rprice"]
+        if(bidder == 1):
+            perc = 0.8
+            bid = v[nb+1] * perc
+            while bid < rprice[i]:
+                perc = perc + 0.1
+                bid = v[nb+1] * perc
+        else:
+            if i < 8:
+                perc = 0.4
+                bid = v[nb+1] * perc
+                while bid < rprice[i]:
+                    perc = perc + 0.1
+                    bid = v[nb+1] * perc
+            else:
+                bid = v[nb+1]
+    if(type == "learn"):
+        v, nb, i = args["v"],args["nb"], args["k"]
+        if(bidder == 1):
+            perc = 0.8
+            bid = v[nb+1] * perc
+        else:
+            if i < 8:
+                perc = 0.4
+                bid = v[nb+1] * perc
+            else:
+                bid = v[nb+1]
+
+    if(type == "trial"):
+        v1, v2, nb_1, nb_2, i, bid1, bid2, rprice = args["v1"],args["v2"], args["nb_1"], args["nb_2"], args["k"], args["bid1"], args["bid2"], args["rprice"]
+        if i < 1:
+            if(bidder == 1):
+                perc = 0.6 
+                bid = v1[nb_1+1] * perc
+                while bid < rprice[i]:
+                    perc = perc + 1
+                    bid = v1[nb_1+1] * perc
+                bid1[i] = bid
+            else:
+                perc = 0.6 
+                bid = v2[nb_2+1] * perc
+                while bid < rprice[i]:
+                    perc = perc + 1
+                    bid = v2[nb_2+1] * perc
+                bid2[i] = bid
+
+        else:
+            if(bidder == 1):
+                if (bid1[i-1] > bid2[i-1]):
+                    perc = 0.6
+                    bid = v1[nb_1+1] * perc
+                    while (bid <= bid2[i-1]) and (bid <= v1[nb_1+1]) and (bid < rprice[i]):
+                        perc = perc + 0.1
+                        bid = v1[nb_1+1] * perc
+                    bid1[i] = bid
+
+                else:
+                    bid = v1[nb_1+1]
+                    bid1[i] = bid
+
+            else:
+                if bid2[i-1] > bid1[i-1]:
+                    perc = 0.6
+                    bid = v2[nb_2+1] * perc
+                    while (bid <= bid1[i-1]) and (bid <= v2[nb_2+1]) and (bid < rprice[i]):
+                        perc = perc + 0.1
+                        bid = v2[nb_2+1] * perc
+                    bid2[i] = bid
+                else:
+                    bid = v2[nb_2+1]
+                    bid2[i] = bid
+    return bid
+
+def learn(v1, v2, T):
+    value_1 = [0 for j in range(T+1)] #initialization of vectors
+    value_2 = [0 for j in range(T+1)]
+    for i in range(T + 1):
+        value_1[i] = v1[i] * 0.8
+        if T < 8:
+            value_2[i] = v2[i] * 0.4
+        else:
+            value_2[i] = v2[i]
+    return value_1, value_2
+
+def graph(bid_1,bid_2,v1,v2):
+    plt.figure()
+    plt.plot(bid_1)
+    plt.plot(bid_2)
+    plt.figure()
+    plt.plot(v1[1:])
+    plt.plot(v2[1:])
+    plt.show()
